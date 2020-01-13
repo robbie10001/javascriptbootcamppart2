b54145232dac5e35b9b6e5c2333e652f02af8b75
Robbie Colborne <robbie_c1000@hotmail.com>
2020-01-13 20:50:12 +1100

    section goals

--- section10_Apply_Functions_To_Collections_Of_Data.js
+++ section10_Apply_Functions_To_Collections_Of_Data.js
@@ -1,8 +1,27 @@
+/*
+GOALS FOR THE SECTION
+
+Use the new arrow function syntax
+Understand and user the following methods
+forEach
+map
+filter
+find
+reduce
+some
+every
+*/ 
 
 /*
 INTRODUCTION TO ARRAY CALLBACK METHODS
 
-
+In this section we are going to look at some of the most useful methods that expect us to put a function in.
+This section is focused on array methods that expect a callback. 
+Arrays come with many built-in methods that accept callback functions.
+Now that we've covered callbacks, we can learn these methods. They are super useful! 
+The general pattern here is that we are going to run a function once per element in each array. 
+each method will have different goals. 
+The methods we see here are going to be used all the time. 
 */ 
 
 /*
