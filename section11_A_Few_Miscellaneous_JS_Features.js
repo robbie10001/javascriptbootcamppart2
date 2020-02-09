2fab6c6e2a0ad3672508c237087a2758e27c696e
Robbie Colborne <robbie_c1000@hotmail.com>
2020-02-10 10:24:52 +1100

    Section11-BoilerPlate

---
+++ section11_A_Few_Miscellaneous_JS_Features.js
@@ -0,0 +1,71 @@
+/*
+NEW JS FEATURES INTRO
+
+
+
+*/ 
+
+
+/*
+DEFAULT PARAMETERS 
+
+*/ 
+
+
+/*
+SPREAD FOR FUNCTION CALLS 
+
+*/ 
+
+
+/*
+SPREAD IN ARRAY LITERALS  
+
+*/ 
+
+
+
+/*
+SPREAD IN OBJECT LITERALS  
+
+*/ 
+
+
+/*
+THE ARGUMENTS OBJECT 
+
+*/ 
+
+
+/*
+REST PARAMETERS 
+
+*/ 
+
+
+
+/*
+DESTRUCTURING ARRAYS 
+
+*/ 
+
+
+/*
+DESTRUCTURING OBJECTS 
+
+*/ 
+
+
+
+/*
+NESTED DESTRUCTURING 
+
+*/ 
+
+
+
+/*
+DESTRUCTURING PARAMETERS 
+
+*/ 
+
