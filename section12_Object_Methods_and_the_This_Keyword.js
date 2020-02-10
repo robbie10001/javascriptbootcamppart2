bcb81fdbd637cb33c693df7042cbd513a66bc103
Robbie Colborne <robbie_c1000@hotmail.com>
2020-02-10 22:12:18 +1100

    section 12 boilterplate ready

---
+++ section12_Object_Methods_and_the_This_Keyword.js
@@ -0,0 +1,62 @@
+/*
+Shorthand Object Properties 
+
+*/ 
+
+
+/*
+Computed Properties 
+
+*/ 
+
+/*
+Adding Methods To Objects 
+
+*/ 
+
+
+/*
+Method Shorthand Syntax
+
+
+*/
+ 
+
+
+/*
+Intro to keyword THIS 
+
+*/ 
+
+
+/*
+Using THIS in methods 
+
+
+*/ 
+
+
+/*
+THIS: Invocation Context
+
+*/ 
+
+
+/*
+Annoyomatic DEMO
+
+*/ 
+
+
+/*
+Putting It All Together: Deck of Cards 
+
+*/
+
+
+/*
+Creating A Deck Factory 
+
+*/
+
+
