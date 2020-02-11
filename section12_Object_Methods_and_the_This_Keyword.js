14e935001e1dee8b6ed0f1c9b98aeedd5df66b3d
Robbie Colborne <robbie_c1000@hotmail.com>
2020-02-11 17:02:30 +1100

    computer properties

--- section12_Object_Methods_and_the_This_Keyword.js
+++ section12_Object_Methods_and_the_This_Keyword.js
@@ -68,7 +68,49 @@ console.log(statal)//{ max: 5, min: 2.8, sum: 26.74, avg: 3.82 }
 /*
 Computed Properties 
 
+Computed properties are an improvement, for the object literal syntax. 
+We can add a property with a dynamic key. 
+
 */ 
+const role = "host"; 
+const person = "James Holland"; 
+const role2 = "Director";
+const person2= "James Cameron"
+//if we try and set our object to host: James Holland using just an object this will not work.
+//for example, 
+const team = {
+    role: person
+}
+//this will evaluate to the object {role: "James Holland"}
+//person is evaluated, while role is not. 
+//In the pass to make this a dynamic value, we have done the following. 
+const teams = {};
+teams[role] = person; 
+teams[role2] = person2; 
+console.log(teams)//{ host: 'James Holland', Director: 'James Cameron' }
+//With the new syntax for computered properties, we can do this all at once. 
+//we can use a variable as a key name in an object literal property! 
+const teamer = { 
+    [role]: person,
+    [role2]: person2,
+    [1+6+9]: "sixteen", 
+};
+console.log(teamer)//{ '16': 'sixteen',host: 'James Holland', Director: 'James Cameron' }
+
+//we
+function addProp(obj, k, v){
+    const copy = {...obj};
+    copy[k] = v;
+    return copy;//{ role: 'James Holland', happy: ':)' }
+}
+const res = addProp(team, "happy", ":)")
+//we could write this all in one line! 
+const addProps = (obj, k, v) => {
+    return {
+        ...obj, [k]: v 
+    }
+}
+const rest = addProp(team, "happy", ":)")
 
 /*
 Adding Methods To Objects 
