91c8665b63911c1f5c8cee447f7660d665f4ae42
Robbie Colborne <robbie_c1000@hotmail.com>
2020-02-12 18:05:56 +1100

    started section 13

--- section13_JS_in_the_browser_DOM_Manipulation.js
+++ section13_JS_in_the_browser_DOM_Manipulation.js
@@ -1,10 +1,22 @@
 /*
 INTRODUCTION TO THE DOM
 
+Through the DOM we connect Javascript to HTML and CSS. 
+This is where JS becomes more visual and we can make stuff that we can actually see! 
+There is a lot to cover in terms of the DOM! 
 
-*/ 
+Javascript helps to give our webpages an interactive element! 
 
-/*
+Javascript in a way gives our websites a life! and can help to make our pages interactive and more user friendly! 
+
+Javascript can be used to put logic into our applications. 
+
+All the interactivity on our websites can be achieved through the use of Javascript.
+
+Javascript and the DOM in very exciting stuff. 
+
+This is where we start to take HTML and CSS and play with it using javascript. 
+
 IMPORTANT NOTE: HTML & CSS
 
 
