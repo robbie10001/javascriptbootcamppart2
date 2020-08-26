3fe13a042dbfc94c57664340bce9d819a06d4213
Robbie Colborne <robbie_c1000@hotmail.com>
2020-08-26 21:39:19 +1000

    intro to the async keyword

--- code/section18_Async_Await_JS_Magic.js
+++ code/section18_Async_Await_JS_Magic.js
@@ -1,8 +1,15 @@
 /*
 A Quick Overview of Async Functions 
-
-
 */ 
+//if we write a function like this, we can't actually log the data variable.
+//Thie is because we have to wait for the data to come back from the server. 
+//our data variale will log before any information is actually recieved from the server.
+function getData() {
+    const data = axios.get('http://swapi.dev/api/planets');
+    console.log(data)
+}
+//it would be nice to not have to use callbacks and .then to resolve promises. 
+//This is where async and await come into play.
 
 
 
--- section18_Asyn&Await_JS_Magic.md
+++ section18_Asyn&Await_JS_Magic.md
@@ -1,5 +1,11 @@
+
 ### A Quick Overview of Async Functions 
 
+Async functions and this section depend significantly on promises. At their core, Async functions are just nice syntactical sugar for promises. It's a nice clear syntax to work with promises.
+
+The Async and Await keywords don't give us anything we can't aleady do using promises. Although they make it easier and clearner to work with promises. 
+
+![images](/images/section18/asyncintro1.png)
 
 
 
