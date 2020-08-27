77a4172bfc3cb10914f5e1c2126f5b618b6679e0
Robbie Colborne <robbie_c1000@hotmail.com>
2020-08-27 13:34:16 +1000

    The Async Keyword

--- code/section18_Async_Await_JS_Magic.js
+++ code/section18_Async_Await_JS_Magic.js
@@ -11,14 +11,76 @@ function getData() {
 //it would be nice to not have to use callbacks and .then to resolve promises. 
 //This is where async and await come into play.
 
-
-
 /*
 The Async Keyword 
-
-
 */ 
 
+//we just have a normal function that returns "hello"
+function greet() {
+    return "Hello"
+}
+
+/*
+//however, when we put the async keyword in front of our function declaration, we get a promise. 
+//Not only was the promise returned, it was also resolved, with the value of "Hello".
+//Whatever the value that we are returning is, the promise this is returned from greet,
+//will be resolved with that value.  
+async function greeter() {
+    return "Hello"
+}
+//so we can do something like this, and get the val, which is equal to 'hello'.
+greeter().then((val) => {
+    //this will log, 'Promise Resolved With Hello'
+    console.log("Promise Resolved With ", val);
+})
+//So we can chain on .then because greeter is going to return a promise. 
+*/
+
+
+//if we call this as a regular function, we will see that the logic works.
+//if we call this function with 3 and 4, everything works fine. 
+//however, if we call it with "e" and "t", we recieve the uncaught throw.
+function add(x, y) {
+    if(typeof x !== 'number' || typeof y !== 'number') {
+        throw 'x and y must be numbers';
+//we recieve back: uncaught x and y must be numbers
+    }
+    return x + y;
+}
+//if we run the same code again with an async in front of the function,
+//if it's successful we get a resolved promise with the value we put in (the sum of two numbers)
+//if it doesn't work, we can back a rejected promise.
+//however, we still recieve an uncaught message. 
+//The same message as the function before. 
+async function adder(x, y) {
+    if(typeof x !== 'number' || typeof y !== 'number') {
+        throw 'x and y must be numbers';
+//we recieve back: uncaught x and y must be numbers
+    }
+    return x + y;
+}
+
+async function adding(x, y) {
+    if(typeof x !== 'number' || typeof y !== 'number') {
+        throw 'x and y must be numbers';
+//we recieve back: uncaught x and y must be numbers
+    }
+    return x + y;
+}
+
+//we can call our function, then put on our .then and .catch.
+//because adding is now an async function, it returns a promise. 
+//and now we are handling it if it's rejected or if its resolved. 
+adding('e', 5)
+    .then((value) => {
+        console.log('Promise resolved with', value);
+    })
+//if we run adding('e', 'p') for example, we 
+    .catch((error) => {
+        console.log('Promise resolved with', error);
+    });
+
+//2 MINUTES 30
 
 
 
--- section18_Asyn&Await_JS_Magic.md
+++ section18_Asyn&Await_JS_Magic.md
@@ -8,9 +8,31 @@ The Async and Await keywords don't give us anything we can't aleady do using pro
 ![images](/images/section18/asyncintro1.png)
 
 
-
 ### The Async Keyword 
 
+Async is a javascript keyword. Async functions always return a promise. If the function returns a value, the promise will be resolved with that value. If they function thrown an exception, the promise will be reject. 
+
+![images](/images/section18/asynckeyword1.png)
+
+The key thing to understand about using the async keyword is that when you put it in front of a function, the function behaves differently. It will now return a new promise. 
+
+![images](/images/section18/asynckeyword2.png)
+
+The next thing we want to talk about is how do we return a promise that is not resolved?
+
+Sometimes we want to make promies that might be rejected or might be resolved. 
+
+Inside of an async function, if we want to return a rejected promise, all we have to do is raise an exception. 
+
+![images](/images/section18/asynckeyword3.png)
+
+An Async function on one level, is just a short cut syntax to make a function that returns a promise. 
+
+If we return a value, that value will be the value that a promise is resolved with. 
+
+If you thrown an exception, instead that promise will be rejected with that value. 
+
+This is not all we need to know about async functions. This is the async keyword. Next we will see the await keyword that makes things even more magical. 
 
 
 
